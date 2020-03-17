a6778daadbf6509c9bf5dd2b641990d885788283
qingxue0606 <77055664@qq.com>
2020-03-17 15:58:43 +0800

    first

---
+++ Controllers/POBrowserController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+
+namespace NetCoreSamples5.Views
+{
+    public class POBrowserController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public POBrowserController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Word()
+        {
+            PageOfficeNetCore.PageOfficeCtrl pageofficeCtrl = new PageOfficeNetCore.PageOfficeCtrl(Request);
+            pageofficeCtrl.ServerPage = "../PageOffice/POServer";
+            pageofficeCtrl.SaveFilePage = "POSaveDoc";
+            pageofficeCtrl.WebOpen("../POBrowser/doc/test.doc", PageOfficeNetCore.OpenModeType.docNormalEdit, "tom");
+            ViewBag.POCtrl = pageofficeCtrl.GetHtmlCode("PageOfficeCtrl1");
+            return View();
+        }
+
+        public async Task<ActionResult> POSaveDoc()
+        {
+            PageOfficeNetCore.FileSaver fs = new PageOfficeNetCore.FileSaver(Request, Response);
+            await fs.LoadAsync();
+            string webRootPath = _webHostEnvironment.WebRootPath;
+            string contentRootPath = _webHostEnvironment.ContentRootPath;
+
+            fs.SaveToFile(webRootPath + "/POBrowser/doc/" + fs.FileName);
+            await Response.Body.WriteAsync(Encoding.ASCII.GetBytes(fs.FileName));
+            fs.ShowPage(300, 300);
+            fs.Close();
+            return Content("OK");
+        }
+    }
+}
---
+++ Controllers/PageOfficeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+
+namespace NetCoreSamples5.Controllers
+{
+    public class PageOfficeController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public PageOfficeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public ActionResult POServer()
+        {
+            PageOfficeNetCore.POServer.Server poServer = new PageOfficeNetCore.POServer.Server(Request, Response);
+            poServer.LicenseFilePath = _webHostEnvironment.ContentRootPath + "/Views/PageOffice/";
+            poServer.Run();
+            return Content("OK");
+        }
+
+        //for debug only
+        //public ActionResult test()
+        //{
+        //    string contentRootPath = _webHostEnvironment.ContentRootPath;
+        //    return Content(contentRootPath);
+        //}
+    }
+}
