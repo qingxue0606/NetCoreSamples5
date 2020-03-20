3e1958cf751f9cd56921f94105f6189543d18c61
qingxue0606 <77055664@qq.com>
2020-03-20 17:18:38 +0800

    2.14
    Signed-off-by: qingxue0606 <77055664@qq.com>

---
+++ Controllers/CreateWord/CreateWordController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreSamples5.Controllers.CreateWord
+{
+    public class CreateWordController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
