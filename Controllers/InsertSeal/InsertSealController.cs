598e1c06bd925c685c32f62242de476ecd16c2c0
qingxue0606 <77055664@qq.com>
2020-05-13 17:20:09 +0800

    1 Signed-off-by: qingxue0606 <77055664@qq.com>

---
+++ Controllers/InsertSeal/InsertSealController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreSamples5.Controllers.InsertSeal
+{
+    public class InsertSealController : Controller
+    {
+
+
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public InsertSealController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+            public IActionResult refresh()
+        {
+
+
+            // 复制服务器端的模板文件命名为新的文件名
+            string webRootPath = _webHostEnvironment.WebRootPath;
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Word\\AddSeal1\\word1_bak.doc", webRootPath + "\\InsertSeal\\Word\\AddSeal1\\word1.doc", true);
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Word\\AddSeal2\\word2_bak.doc", webRootPath + "\\InsertSeal\\Word\\AddSeal2\\word2.doc", true);
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Word\\AddSeal3\\word3_bak.doc", webRootPath + "\\InsertSeal\\Word\\AddSeal3\\word3.doc", true);
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Word\\AddSeal4\\word4_bak.doc", webRootPath + "\\InsertSeal\\Word\\AddSeal4\\word4.doc", true);
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Word\\AddSeal5\\test_bak.doc", webRootPath + "\\InsertSeal\\Word\\AddSeal5\\test.doc", true);
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Word\\AddSeal6\\test_bak.doc", webRootPath + "\\InsertSeal\\Word\\AddSeal6\\test.doc", true);
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Word\\AddSeal7\\test_bak.doc", webRootPath + "\\InsertSeal\\Word\\AddSeal7\\test.doc", true);
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Word\\AddSeal8\\word8_bak.doc", webRootPath + "\\InsertSeal\\Word\\AddSeal8\\word8.doc", true);
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Word\\AddSeal9\\word9_bak.doc", webRootPath + "\\InsertSeal\\Word\\AddSeal9\\word9.doc", true);
+
+
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Word\\AddSign1\\word1_bak.doc ", webRootPath + "\\InsertSeal\\Word\\AddSign1\\word1.doc ", true);
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Word\\AddSign2\\test_bak.doc ", webRootPath + "\\InsertSeal\\Word\\AddSign2\\test.doc ", true);
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Word\\AddSign3\\test_bak.doc ", webRootPath + "\\InsertSeal\\Word\\AddSign3\\test.doc ", true);
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Word\\AddSign4\\test_bak.doc ", webRootPath + "\\InsertSeal\\Word\\AddSign4\\test.doc ", true);
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Word\\AddSign5\\word_bak.doc ", webRootPath + "\\InsertSeal\\Word\\AddSign5\\word.doc ", true);
+
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Word\\BatchAddSeal\\doc\\bak\\test1.doc ", webRootPath + "\\InsertSeal\\Word\\BatchAddSeal\\doc\\test1.doc ", true);
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Word\\BatchAddSeal\\doc\\bak\\test2.doc ", webRootPath + "\\InsertSeal\\Word\\BatchAddSeal\\doc\\test2.doc ", true);
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Word\\BatchAddSeal\\doc\\bak\\test3.doc", webRootPath + "\\InsertSeal\\Word\\BatchAddSeal\\doc\\test3.doc ", true);
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Word\\BatchAddSeal\\doc\\bak\\test4.doc", webRootPath + "\\InsertSeal\\Word\\BatchAddSeal\\doc\\test4.doc ", true);
+
+
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Excel\\AddSeal1\\test_bak.xls ", webRootPath + "\\InsertSeal\\Excel\\AddSeal1\\test.xls ", true);
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Excel\\AddSeal2\\test_bak.xls ", webRootPath + "\\InsertSeal\\Excel\\AddSeal2\\test.xls ", true);
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Excel\\AddSeal3\\test_bak.xls ", webRootPath + "\\InsertSeal\\Excel\\AddSeal3\\test.xls ", true);
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Excel\\AddSeal4\\test_bak.xls ", webRootPath + "\\InsertSeal\\Excel\\AddSeal4\\test.xls ", true);
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Excel\\AddSeal5\\test_bak.xls ", webRootPath + "\\InsertSeal\\Excel\\AddSeal5\\test.xls ", true);
+
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Excel\\AddSign1\\test_bak.xls ", webRootPath + "\\InsertSeal\\Excel\\AddSign1\\test.xls ", true);
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Excel\\AddSign2\\test_bak.xls ", webRootPath + "\\InsertSeal\\Excel\\AddSign2\\test.xls ", true);
+            System.IO.File.Copy(webRootPath + "\\InsertSeal\\Excel\\AddSign3\\test_bak.xls ", webRootPath + "\\InsertSeal\\Excel\\AddSign3\\test.xls ", true);
+
+            return Redirect("/InsertSeal/");
+        }
+    }
+}
---
+++ Controllers/InsertSeal/Word/AddSealController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreSamples5.Controllers.InsertSeal.Word
+{
+    
+    public class AddSealController : Controller
+    {
+
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public AddSealController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+        [Route("InsertSeal/Word/AddSeal/Word1")]
+        public IActionResult Word1()
+        {
+            PageOfficeNetCore.PageOfficeCtrl pageofficeCtrl = new PageOfficeNetCore.PageOfficeCtrl(Request);
+            pageofficeCtrl.ServerPage = "/PageOffice/POServer";
+
+            //添加自定义按钮
+            pageofficeCtrl.AddCustomToolButton("保存", "Save", 1);
+            pageofficeCtrl.AddCustomToolButton("加盖印章", "InsertSeal()", 2);
+            pageofficeCtrl.AddCustomToolButton("删除印章", "DeleteSeal()", 21);
+            pageofficeCtrl.AddCustomToolButton("验证印章", "VerifySeal()", 5);
+            pageofficeCtrl.AddCustomToolButton("修改密码", "ChangePsw()", 0);
+
+            //设置保存页面
+            pageofficeCtrl.SaveFilePage = "SaveDoc";
+            //打开Word文档
+            pageofficeCtrl.WebOpen("/InsertSeal/Word/AddSeal1/word1.doc", PageOfficeNetCore.OpenModeType.docAdmin, "tom");
+            ViewBag.POCtrl = pageofficeCtrl.GetHtmlCode("PageOfficeCtrl1");
+
+            return View();
+        }
+
+
+        public async Task<ActionResult> SaveDoc()
+        {
+            PageOfficeNetCore.FileSaver fs = new PageOfficeNetCore.FileSaver(Request, Response);
+            await fs.LoadAsync();
+            string webRootPath = _webHostEnvironment.WebRootPath;
+            fs.SaveToFile(webRootPath + "/InsertSeal/Word/AddSeal1/" + fs.FileName);
+            fs.Close();
+            return Content("OK");
+        }
+
+
+    }
+}
